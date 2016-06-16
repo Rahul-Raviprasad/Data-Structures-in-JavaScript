24e79a4ebdc30970ce8a45393da76e452fe70d6b
Rahul Raviprasad <rahul.raviprasad@gmail.com>
2016-06-16 09:59:26 -0700

    added method to remove node from a BST

--- BinaryTree/bst.js
+++ BinaryTree/bst.js
@@ -34,6 +34,8 @@ function BST() {
   this.getMin = getMin;
   this.getMax = getMax;
   this.find = find;
+  this.remove = remove;
+  this.removeNode = removeNode;
 }
 
 function insert(data) {
@@ -125,4 +127,39 @@ function find(data) {
     }
   }
   return current;
+}
+
+function remove(data) {
+  root = removeNode(this.root, data);
+}
+
+function removeNode(node, data) {
+  if (node == null) {
+    return null;
+  }
+  if (data == node.data) {
+    // node has no children
+    if (node.left == null && node.right == null) {
+      return null;
+    }
+    // node has no left child
+    if (node.left == null) {
+      return node.right;
+    }
+    // node has no right child
+    if (node.right == null) {
+      return node.left;
+    }
+    // node has two children
+    var tempNode = getSmallest(node.right);
+    node.data = tempNode.data;
+    node.right = removeNode(node.right, tempNode.data);
+    return node;
+  } else if (data < node.data) {
+    node.left = removeNode(node.left, data);
+    return node;
+  } else {
+    node.right = removeNode(node.right, data);
+    return node;
+  }
 }
