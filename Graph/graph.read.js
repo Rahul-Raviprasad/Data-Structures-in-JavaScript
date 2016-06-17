3bcaf2766f34b6f314d4288a07d206d8b0307d66
Rahul Raviprasad <rahul.raviprasad@gmail.com>
2016-06-17 04:46:27 -0700

    minor change

--- Graph/graph.read.js
+++ Graph/graph.read.js
@@ -4,5 +4,5 @@ The efficiency of the graph algorithms largely depends on the data structure
 used to represent it.
 
 Graph Definitions
-
+A graph consists of a set of vertices and a set of edges.
 */
